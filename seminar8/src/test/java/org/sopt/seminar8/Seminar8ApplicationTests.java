5447b80447729d20c7a0e787eb4d415c4c1d3354
hyunjong_96 <ahajongs@gmail.com>
2021-02-10 20:16:16 +0900

    #8 spring data JPA init

---
+++ seminar8/src/test/java/org/sopt/seminar8/Seminar8ApplicationTests.java
@@ -0,0 +1,13 @@
+package org.sopt.seminar8;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Seminar8ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
